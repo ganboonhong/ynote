78d5413e5feb96a9892ff39c95fc3114d200b855
ganboonhong <ganboonhong@gmail.com>
2015-12-29 14:55:14 +0800

    updating create.blade.php

--- resources/views/admin/article/create.blade.php
+++ resources/views/admin/article/create.blade.php
@@ -1,38 +1,38 @@
 @extends('admin.templates.create')
 
     @section('title')
-        文章（新增）
+        Creat a new article
     @stop
 
     @section('title')
-        文章（新增）
+        Creat a new article
     @stop
 
     @section('content')
 
         {!! Form::open(array('url' => 'admin/article', 'id' => 'article_form')) !!}
             <div class="form-group">
-                <label for="name">標題:</label>
+                <label for="name">Title:</label>
                 <input type="text" name="title" class="form-control" id="title">
             </div>
 
             <div class="form-group">
-                <label for="name">標題(英文):</label>
+                <label for="name">Title(Eng):</label>
                 <input type="text" name="title_en" class="form-control" id="title_en">
             </div>
 
             <div class="nice_editor form-group">
-                <label for="name">中文內容:</label>
+                <label for="name">Content:</label>
                 <textarea name="content" id="content" style="width:100%"></textarea>
             </div>
 
             <div class="nice_editor form-group">
-                <label for="name">英文內容:</label>
+                <label for="name">Content(Eng)</label>
                 <textarea name="content_en" id="content_en" style="width:100%" class="nice_editor"></textarea>
             </div>
 
             <div class="form-group">
-                <label for="sel1">分類：</label>
+                <label for="sel1">Category：</label>
                 <select name="category_id" class="form-control" id="sel1">
                     @foreach( $categories as $category )
                         <option value="{{$category->category_id}}">{{$category->name}}</option>
