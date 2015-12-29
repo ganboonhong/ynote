3017f43b10432ff5bae70986d7a1b2801e3b191e
ganboonhong <ganboonhong@gmail.com>
2015-12-29 16:51:26 +0800

    updating article/create.blade.php

--- resources/views/admin/partials/footer_elements.blade.php
+++ resources/views/admin/partials/footer_elements.blade.php
@@ -11,4 +11,4 @@
     <label for="invisible">No</label>
     <input name="visible" id="invisible" type="radio" value="N">
 </div>
-<button type="submit" class="btn btn-primary form-control" style="margin: 10px 0px 20px 0px">新增</button>
+<button type="submit" class="btn btn-primary form-control" style="margin: 10px 0px 20px 0px">Create</button>
