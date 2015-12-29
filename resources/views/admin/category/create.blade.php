f9736a2ae5ceb85a5b9cdc2bbe81f6b140c9bbe2
ganboonhong <ganboonhong@gmail.com>
2015-12-29 15:03:52 +0800

    updating views

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -1,11 +1,11 @@
 @extends('admin.templates.create')
 
     @section('title')
-        文章分類（新增）
+        Creating a new category for article
     @stop
 
     @section('title')
-        文章分類（新增）
+        Creating a new category for article
     @stop
 
     @section('content')
@@ -13,12 +13,12 @@
     {!! Form::open(array('url' => 'admin/category', 'id' => 'category_form')) !!}
 
     <div class="form-group">
-        <label for="name">名稱:</label>
+        <label for="name">Name:</label>
         <input type="text" name="name" class="form-control" id="name">
     </div>
 
     <div class="form-group">
-        <label for="name_en">英文名稱:</label>
+        <label for="name_en">Name(Eng):</label>
         <input type="text" name="name_en" class="form-control" id="name_en">
     </div>
 
--- resources/views/admin/category/edit.blade.php
+++ resources/views/admin/category/edit.blade.php
@@ -1,28 +1,29 @@
 @extends('admin.templates.create')
 
 @section('title')
-    文章分類（修改）
+    Editing category
 @stop
 
 @section('title')
-    文章分類（修改）
+    Editing category
 @stop
 
 @section('content')
 
     {!! Form::open(array('route' => array('category_update', $category->category_id))) !!}
     <div class="form-group">
-        <label for="name">名稱:</label>
+        <label for="name">Name:</label>
         <input type="text" name="name" class="form-control" id="name" value="{{$category->name}}">
     </div>
 
     <div class="form-group">
-        <label for="name_en">英文名稱:</label>
+        <label for="name_en">Name(Eng):</label>
         <input type="text" name="name_en" class="form-control" id="name_en" value="{{$category->name_en}}">
     </div>
 
+    @include('admin.partials.footer_elements')
 
-    <div style="margin-bottom: 20px;">
+    {{--<div style="margin-bottom: 20px;">
         公開
         <span style="margin-left:10px ;margin-right:10px ; ">
             <label for="visible_Y">是</label>
@@ -32,7 +33,7 @@
         <label for="visible_N">否</label>
         <input name="visible" id="visible_N" type="radio" value="N"
                     @if($category->visible == 'N') {{'checked'}} @endif>
-    </div>
+    </div>--}}
 
     <button type="submit" class="btn btn-primary form-control">修改</button>
     {!! Form::close() !!}
