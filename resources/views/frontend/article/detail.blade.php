e810ed226bef8022719397906028d489744a03ba
ganboonhong <ganboonhong@gmail.com>
2016-01-23 01:53:11 +0800

    testing fb share

--- resources/views/frontend/article/detail.blade.php
+++ resources/views/frontend/article/detail.blade.php
@@ -18,5 +18,13 @@
                 @endif
             </p>
         </div>
+
+
+        <div
+                class="fb-like"
+                data-share="true"
+                data-width="450"
+                data-show-faces="true">
+        </div>
     </div>
 @stop
