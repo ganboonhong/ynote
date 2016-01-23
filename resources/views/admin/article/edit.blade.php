6f4002f64a117d1274387a831be3fb9c736f5915
ganboonhong <ganboonhong@gmail.com>
2016-01-23 17:33:18 +0800

    testing article/edit.blade.php

--- resources/views/admin/article/edit.blade.php
+++ resources/views/admin/article/edit.blade.php
@@ -21,10 +21,10 @@
                 <input type="text" name="title_en" class="form-control" id="title_en" value="{{$article->title_en}}">
             </div>--}}
 
-            {{--<div class="form-group">
+            <div class="form-group">
                 <div>
                     @if($article->list_pic != "")
-                        --}}{{--<img src='/uploads/{{$article->list_pic}}' style="width: 100px;">--}}{{--
+                        {{--<img src='/uploads/{{$article->list_pic}}' style="width: 100px;">--}}
                         <img src='{{json_decode($article->cloudinary_api_response)->url}}' style="width: 100px;">
                     @endif
                 </div>
@@ -35,89 +35,14 @@
             <div class="nice_editor form-group">
                 <label for="name">Content:</label>
                 <textarea name="content" id="content" style="width:100%">{{$article->content}}</textarea>
-            </div>--}}
+            </div>
 
             {{--<div class="nice_editor form-group">
                 <label for="name">Content (Eng)</label>
                 <textarea name="content_en" id="content_en" style="width:100%" class="nice_editor">{{$article->content_en}}</textarea>
             </div>--}}
 
-            <div class="form-group">
-                <label for="sel1">Category：</label>
-                <select name="category_id" class="form-control" id="sel1">
-                    @foreach( $categories as $category )
-                        <option value="{{$category->category_id}}"
-                                @if($category->category_id == $article->category_id) selected @endif
-                                >{{$category->name}}</option>
-                    @endforeach
-                </select>
-            </div>
 
-            <div class="form-group">
-                <label for="sel1">Function Type：</label>
-                <select name="admin_function_type_id" class="form-control">
-                    @foreach( $function_types as $function_type )
-                        <option value="{{$function_type->admin_function_type_id}}"
-                                @if($function_type->admin_function_type_id == $article->admin_function_type_id) selected @endif
-                                >{{$function_type->name}}</option>
-                    @endforeach
-                </select>
-            </div>
-
-            <div class="form-group">
-                <label for="reference">Reference:</label>
-                <input type="text" name="reference" class="form-control" id="reference"  value="{{$article->reference}}">
-            </div>
-
-            <div class="form-group">
-                <label for="priority">Priority:</label>
-                <input type="text" name="sort" class="form-control" id="priority"  value="{{$article->sort}}">
-            </div>
-
-            <table style="width:30%">
-                <tr>
-                    <td>Visible:</td>
-                    <td>
-                        <label for="visible">Yes</label>
-                        <input name="visible" id="visible" type="radio" value="Y"
-                               @if($article->visible == 'Y') checked @endif>
-                    </td>
-                    <td>
-                        <label for="invisible">No</label>
-                        <input name="visible" id="invisible" type="radio" value="N"
-                               @if($article->visible == 'N') checked @endif>
-                    </td>
-                </tr>
-
-                {{--<tr>
-                    <td>Chinese Version:</td>
-                    <td>
-                        <label for="version_cht_yes">Yes</label>
-                        <input name="version_cht" id="version_cht_yes" type="radio" value="Y"
-                               @if($article->version_cht == 'Y') checked @endif>
-                    </td>
-                    <td>
-                        <label for="version_cht_no">No</label>
-                        <input name="version_cht" id="version_cht_no" type="radio" value="N"
-                               @if($article->version_cht == 'N') checked @endif>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td>English Version:</td>
-                    <td>
-                        <label for="version_en_yes">Yes</label>
-                        <input name="version_en" id="version_en_yes" type="radio" value="Y"
-                               @if($article->version_en == 'Y') checked @endif>
-                    </td>
-                    <td>
-                        <label for="version_en_no">No</label>
-                        <input name="version_en" id="version_en_no" type="radio" value="N"
-                               @if($article->version_en == 'N') checked @endif>
-                    </td>
-                </tr>--}}
-
-            </table>
 
         <button type="submit" class="btn btn-primary form-control" style="margin: 10px 0px 20px 0px">Done</button>
 
