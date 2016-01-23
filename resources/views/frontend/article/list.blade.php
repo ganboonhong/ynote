12c23eacc4f7b57fe7553479fb88b414935531b0
ganboonhong <ganboonhong@gmail.com>
2016-01-23 21:12:19 +0800

    updating frontend/list.blade.php

--- resources/views/frontend/article/list.blade.php
+++ resources/views/frontend/article/list.blade.php
@@ -9,7 +9,7 @@
         <div class="col-md-9 col-sm-12 col-xs-12 list-wrapper">
             <div class="list-item-container">
                 @foreach($articles as $article)
-                    <div class="col-md-4 col-sm-5 col-xs-6 item">
+                    <div class="col-md-4 col-sm-5 col-xs-12 item">
                         <a href="{{route('article_detail',[$article->article_id, $article->category_id, $user->user_id])}}">
                             {{--<img src="../uploads/{{$article->list_pic}}" class="list-pics" />--}}
                             <img src="{{json_decode($article->cloudinary_api_response)->url}}" class="list-pics" />
